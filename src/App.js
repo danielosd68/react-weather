96694b004b3daddd5a68a82b084d3d08791806d4
Daniel <danielosd68@gmail.com>
2022-08-22 18:53:03 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/App.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-
+import Weather from './components/weather';
+const API = "5f924e66209d86f699933ccbf1072991";
 
 function App() {
+  const [weather, setWeather] = useState(false);
+  const [input, setInput] = useState(false);
+  const [isFormActive, setFormActive] = useState(false);
+
+  const checkWeather = (city) => {
+  
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API}`)
+      .then((response) => {
+        console.log(response.promiseStatus);
+        const data = response.json();
+        let city = false;
+        data
+          .then((res) => { city = res[0].name; console.log(city); })
+          .catch(err => console.error(err))
+      })
+  }
+
 
   return (
     <div className="App">
       <div className="icon">
-        <i className="bi bi-binoculars"></i>
+        <i className="bi bi-binoculars" onClick={() => { setFormActive(!isFormActive) }}></i>
       </div>
-      <div className="content">
-        <div className="item">
-          
+      {weather === false ? <h1 className="hello-header">Witaj! Sprawdź aktualną pogodę!</h1> : <>
+
+        <div className="content">
+          <Weather city={weather} />
         </div>
-        <div className="item"></div>
-      </div>
+      </>}
+      {isFormActive !== false ? <div className="form">
+        <form>
+          <input className="city" name="city" onChange={(e) => { setInput(e.target.value) }} />
+          <button onClick={(e) => { e.preventDefault(); checkWeather(input) }}>Sprawdź pogodę</button>
+        </form>
+      </div> : ""}
     </div>
-  );
+  )
+
 }
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,10 +1,30 @@
+import PropTypes from "prop-types";
+
 const Weather = (props) => {
     return (
         <>
-            <h1></h1>
-            <div className="weather-info"></div>
+            <div className="item">
+                <h1>Miejscowość: {props.city}</h1>
+                <div className="weather-info">
+                    <div className="weather-info-item">
+                        <h3>{props.description}</h3>
+                    </div>
+                    <div className="weather-info-item">
+                        <p id="temperature">{props.temperature}</p> <p id="st">st. C</p>
+                    </div>
+                </div>
+            </div>
+            <div className="item">
+                <i className="bi bi-cloudy"></i>
+            </div>
         </>
     )
+}
+Weather.propTypes = {
+    city: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    temperature: PropTypes.number.isRequired
+
 }
 
 export default Weather;
