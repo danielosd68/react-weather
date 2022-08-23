fb34636631ef5207dd0036fecae7ac7cc8b76b56
Daniel <danielosd68@gmail.com>
2022-08-23 14:14:01 +0200

    engine

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import { useState, useEffect } from 'react';
 import './styles/App.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import Weather from './components/weather';
+import './logo.svg';
 const API = "5f924e66209d86f699933ccbf1072991";
 
 function App() {
   const [weather, setWeather] = useState(false);
   const [input, setInput] = useState(false);
   const [isFormActive, setFormActive] = useState(false);
+  const [dataStatus, setDataStatus] = useState(false);
 
   const checkWeather = (city) => {
-
+    setWeather(false);
+    setDataStatus(!dataStatus);
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API}`)
       .then((response) => {
-        console.log(response.promiseStatus);
         const data = response.json();
-        let city = false;
+        let city = null;
+        let description = null;
+        let temperature = null;
         data
-          .then((res) => { city = res[0].name; console.log(city); })
+          .then((res) => {
+            city = res[0].name;
+            const lat = res[0].lat;
+            const lon = res[0].lon;
+            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&lang=pl&units=metric`)
+              .then((response) => {
+                const data = response.json();
+
+                data
+                  .then((res) => {
+
+                    const data = {
+                      city: city,
+                      description: res.weather[0].description,
+                      temperature: Math.floor(res.main.temp)
+                    }
+
+                    setWeather(data);
+                    setDataStatus(false);
+                  })
+
+              })
+              .catch((err) => { alert(`Błąd: ${err}`) })
+
+
+          })
           .catch(err => console.error(err))
       })
   }
@@ -25,6 +54,9 @@ function App() {
 
   return (
     <div className="App">
+      {dataStatus !== false ? <div className="loading">
+        <img src="./logo192.png" alt="Loading logo" />
+      </div> : ""}
       <div className="icon">
         <i className="bi bi-binoculars" onClick={() => { setFormActive(!isFormActive) }}></i>
         {isFormActive !== false ? <div className="form">
@@ -36,9 +68,11 @@ function App() {
       </div>
       {weather === false ? <h1 className="hello-header">Witaj! Sprawdź aktualną pogodę!</h1> : <>
 
+
         <div className="content">
-          <Weather city={weather} />
+          <Weather city={weather.city} description={weather.description} temperature={weather.temperature} />
         </div>
+
       </>}
 
     </div>
