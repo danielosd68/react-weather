608035bb6cd394547d6a0dadd6b10e00d0a033a8
Daniel <danielosd68@gmail.com>
2022-08-25 15:55:46 +0200

    New function downloadData

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './styles/App.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import Weather from './components/weather';
 import Error from './components/error';
-import './logo.svg';
-const API = "5f924e66209d86f699933ccbf1072991";
-
+import downloadData from './functions/weathercheck';
 
 
 if (localStorage.getItem('weather-data') === null) {
@@ -29,103 +27,43 @@ function App() {
       setInput(false);
       setWeather(false);
       setDataStatus(!dataStatus);
-      setTimeout(() => {
-        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API}`, { method: 'GET' })
-          .then((response) => {
-            console.log(response);
-            if (response.status !== 200) {
-              console.warn('Zapytanie nie zostało zrealizowane z powodu zewnętrznego błędu.');
-              setError('Napotkano zewnętrzny problem z dostępem do danych. Spróbuj ponownie później!');
-              setDataStatus(false);
-              setWeather('false');
-            }
+      downloadData(input)
+        .then((response) => {
+          setWeather(response);
+          setDataStatus(false);
+        })
+        .catch((error) => {
+          setInput(false);
+          setDataStatus(false);
+          setError(error);
+        })
 
-
-            else {
-              const data = response.json();
-
-              let city = null;
-              let description = null;
-              let temperature = null;
-              data
-                .then((res) => {
-                  console.log(res);
-                  if (res.length === 0) {
-                    console.error('Nie odnaleziono podanej miejscowości');
-                    setError('Nie znaleziono podanej miejscowości. Spróbuj ponownie.');
-                    setDataStatus(false);
-                    setWeather('false');
-                  }
-
-                  else {
-                    city = res[0].name;
-                    const lat = res[0].lat;
-                    const lon = res[0].lon;
-                    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&lang=pl&units=metric`, { method: 'GET' })
-                      .then((response) => {
-                        const data = response.json();
-                        if (response.status !== 200) {
-                          console.error('Zapytanie nie zostało zrealizowane z powodu zewnętrznego błędu.');
-                          setError('Napotkano zewnętrzny problem z dostępem do danych. Spróbuj ponownie później!');
-                          setDataStatus(false);
-                          setWeather('false');
-                        }
-
-                        else {
-                          data
-                            .then((res) => {
-
-                              const data = {
-                                city: city,
-                                description: res.weather[0].description,
-                                temperature: Math.round(res.main.temp),
-                                realfeel: Math.round(res.main.feels_like),
-                                pressure: res.main.pressure,
-                                windspeed: Math.round(res.wind.speed * 3.6),
-
-                              }
-
-                              setWeather(data);
-                              setDataStatus(false);
-                              localStorage.setItem('weather-data', JSON.stringify(data));
-
-
-                            })
-                        }
-
-
-
-                      })
-                  }
-
-
-                })
-            }
-
-
-          })
-      }, 2500)
     }
-
 
   }
 
 
   return (
     <div className="App">
+
       {dataStatus !== false ? <div className="loading">
         <img src="./logo192.png" alt="Loading logo" />
       </div> : ""}
+
       {error !== false ? <Error error={error} exit={() => { setError(false) }} /> : ""}
+
       <div className="icon">
         <i className="bi bi-binoculars" onClick={() => { setFormActive(!isFormActive) }}></i>
+
         {isFormActive !== false ? <div className="form">
           <form>
             <input className="city" name="city" onChange={(e) => { setInput(e.target.value) }} />
             <button onClick={(e) => { e.preventDefault(); checkWeather(input) }}><i className="bi bi-search"></i></button>
           </form>
         </div> : ""}
+
       </div>
+
       {weather == false ? <h1 className="hello-header">Witaj! Sprawdź aktualną pogodę!</h1> : <>
 
 
---
+++ src/functions/weathercheck.js
@@ -0,0 +1,67 @@
+export default function downloadData(city) {
+    const API = "5f924e66209d86f699933ccbf1072991";
+
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API}`)
+            .then((response) => {
+                if (response.status !== 200) {
+                    reject('Napotkano zewnętrzny problem z dostępem do danych. Spróbuj ponownie później!');
+                }
+
+                else {
+                    const data = response.json();
+                    let city = null;
+                    let description = null;
+                    let temperature = null;
+
+                    data
+                        .then((res) => {
+                            if (res.length === 0) {
+                                reject('Nie znaleziono podanej miejscowości. Spróbuj ponownie.');
+                            }
+                            else {
+                                city = res[0].name;
+                                const lat = res[0].lat;
+                                const lon = res[0].lon;
+
+                                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API}&lang=pl&units=metric`)
+                                    .then((response) => {
+                                        const data = response.json();
+
+                                        if (response.status !== 200) {
+                                            reject('Napotkano zewnętrzny problem z dostępem do danych. Spróbuj ponownie później!');
+                                        }
+
+                                        else {
+                                            data
+                                                .then((response) => {
+                                                    const data = {
+                                                        city: city,
+                                                        description: response.weather[0].description,
+                                                        temperature: Math.round(response.main.temp),
+                                                        realfeel: Math.round(response.main.feels_like),
+                                                        pressure: response.main.pressure,
+                                                        windspeed: Math.round(response.wind.speed * 3.6),
+
+                                                    }
+
+                                                    localStorage.setItem('weather-data', JSON.stringify(data));
+
+                                                    resolve(data);
+                                                })
+                                        }
+                                    })
+                            }
+                        })
+
+                }
+            })
+
+    })
+
+
+
+
+
+
+}
